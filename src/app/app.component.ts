f006ebc88118be1b81459a0db893ab86dcaea9e2
Hussam Khan <hussam@idwise.com>
2022-09-27 16:27:04 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const IDWise = (window as any).IDWise;
+
+    IDWise.initialize({
+      businessId: 'test-remotepass-journey', // this is the same as journey definition id
+      locale: 'en',
+    })
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      .then(idwise => {
+        console.log({ idwise });
+        idwise.startJourney({
+          mount: '#idwise-mount',
+          journeyTemplateId: 'test-remotepass-journey', // journey definition id
+          userId: 'verify-websdk-test', // An identifier that uniquely idenfies the user carrying out this journey
+          eventHandlers: {
+            // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+            onFinished(details) {
+              alert('Thanks for completing the registration');
+            },
+          },
+        });
+      });
+  }
 }
